ec095873fb96811723de4d241e52a34e8d270f03
Francisco Vecchia <franvecchia00@gmail.com>
2022-12-19 22:55:55 -0300

    segunda pre-entrega

--- proyecto-prueba.js
+++ proyecto-prueba.js
@@ -1,3 +1,8 @@
+function doble(num) {
+    resultado = num * 2;
+    console.log("El doble del numero es: ", resultado)
+}
+
 function division(divisor, dividendo) {
     resultado = divisor/dividendo;
     console.log("Resultado de la division:", resultado);
@@ -34,42 +39,50 @@ function esPrimo(numero) {
     }
 }
 
+function calcular_raiz (numero, n) {
+    if (numero < 0 ) {
+        console.log("No existe la raiz de un numero negativo (En reales).");
+    }
+
+    return Math.pow(numero,1/n);
+}
+
 function calculadora () {
-    let operacion = parseFloat(prompt("Ingrese la operación que desea realizar (El numero que indique segun lo que quiera realizar): "));
     let cantNumeros;
-    let resultado = 0;
+    let lista_numeros = [];
+    let resultado;
     let i;
+    let operacion = parseFloat(prompt("Ingrese la operación que desea realizar (El numero que indique segun lo que quiera realizar): "));
 
     if (operacion == 1) {
-        let suma;
+        let suma = 0;
         cantNumeros = parseInt(prompt("Ingrese la cantidad de numeros: "));
-        for (i=1;i<=cantNumeros;i++) {
-            suma = parseFloat(prompt("Ingrese los numeros a sumar: "));
-            resultado = resultado + suma;
+        for (i=0;i<cantNumeros;i++) {
+            lista_numeros[i] = parseInt(prompt("Ingrese los numeros a sumar"));
+            suma = lista_numeros[i] + suma;
         }
-        console.log("Resultado de la suma: ", resultado);
+        console.log("Resultado de la suma: ", suma);
     }
     else if (operacion == 2) {
-        let resta;
+        let resta = 0;
         cantNumeros = parseInt(prompt("Ingrese la cantidad de numeros: "));
         for (i=1;i<=cantNumeros;i++) {
-            resta = parseFloat(prompt("Ingrese los numeros a restar: "));
+            lista_numeros[i] = parseFloat(prompt("Ingrese los numeros a restar: "));
             if (i>1) {
-                resta = resta * -1;
+                lista_numeros[i] = lista_numeros[i] * -1;
             }
-            resultado = resultado + resta;
+            resta = lista_numeros[i] + resta;
         }
-        console.log ("Resultado de la resta: ", resultado);
+        console.log ("Resultado de la resta: ", resta);
     }
     else if (operacion == 3) {
-        let multiplicacion;
+        let multiplicacion = 1;
         cantNumeros = parseInt(prompt("Ingrese la cantidad de numeros: "));
-        resultado = 1;
         for (i=1;i<=cantNumeros;i++) {
-            multiplicacion = parseFloat(prompt("Ingrese los numeros a multiplicar: "));
-            resultado = multiplicacion * resultado;
+            lista_numeros[i] = parseFloat(prompt("Ingrese los numeros a multiplicar: "));
+            multiplicacion = lista_numeros[i] * multiplicacion;
         }
-        console.log("Resultado de la multiplicacion:", resultado);
+        console.log("Resultado de la multiplicacion:", multiplicacion);
     }
     else if (operacion == 4) {
         let divisor = parseFloat(prompt("Ingrese el divisor: "));
@@ -82,14 +95,39 @@ function calculadora () {
         potencia(base_potencia,exponente_potencia);
     }
     else if (operacion == 6) {
-        let factorial = parseInt(prompt("Ingrese un numero: "));
+        let factorial = parseInt(prompt("Ingrese un numero para calcularle su factorial: "));
         numeroFactorial(factorial);
     }
     else if (operacion == 7) {
-        let numero = parseInt(prompt("Ingrese un numero: "));
+        let numero = parseInt(prompt("Ingrese un numero para saber si es o no primo: "));
         esPrimo(numero);
     }
+    else if (operacion == 8) {
+        cantNumeros = parseInt(prompt("Ingrese la cantidad de numeros: "));
+        for (i=0;i<cantNumeros;i++) {
+            lista_numeros[i]= parseInt(prompt("Ingrese un numero para doblarlo"));
+        }
+        lista_numeros.forEach(doble);
+    }
+    else if (operacion == 9) {
+        let raiz_numero = parseFloat(prompt("Ingrese un numero para calcularle su raiz: "));
+        let indice_raiz = parseFloat(prompt("Ingrese el indice de la raiz: "));
+        console.log("El resultado es: ", calcular_raiz(raiz_numero, indice_raiz));
+    }
+    else if (operacion == 10) {
+        let valor_exacto = prompt("Ingrese PI o E segun el valor que desee.");
+        if (valor_exacto == "PI" || valor_exacto == "Pi" || valor_exacto == "pi") {
+            resultado = Math.PI;
+        }
+        else if (valor_exacto == "E" || valor_exacto == "e") {
+            resultado = Math.E;
+        }
+        else {
+            console.log("Operacion no encontrada, intentelo nuevamente.");
+        }
+        console.log("Valor exacto: ", resultado);
+    }
     else {
-        console.log("Operacion no encontrada.");
+        console.log("Operacion no encontrada, intentelo nuevamente.");
     }
 }
