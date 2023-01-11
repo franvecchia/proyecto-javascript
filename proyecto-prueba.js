10a8e07a95530596bffc7a190b87c4ef6cd8cfd1
Francisco Vecchia <franvecchia00@gmail.com>
2023-01-11 16:43:54 -0300

    3er pre-entrega

--- proyecto-prueba.js
+++ proyecto-prueba.js
@@ -1,125 +1,34 @@
-let operandoA;
-let operandoB;
-let operacion;
-//variables
-let resultado = document.getElementById("display");
-let reset = document.getElementById("reset");
-let suma = document.getElementById("+");
-let resta = document.getElementById("-");
-let multiplicacion = document.getElementById("*");
-let division = document.getElementById("/");
-let igual = document.getElementById("equal");
-let uno = document.getElementById("1");
-let dos = document.getElementById("2");
-let tres = document.getElementById("3");
-let cuatro = document.getElementById("4");
-let cinco = document.getElementById("5");
-let seis = document.getElementById("6");
-let siete = document.getElementById("7");
-let ocho = document.getElementById("8");
-let nueve = document.getElementById("9");
-let cero = document.getElementById("0");
+function insertar(num) {
+    let numero = document.getElementById("display").innerHTML;
+    document.getElementById("display").innerHTML = numero + num;
+}
 
-    uno.addEventListener("click", function(){
-        resultado.textContent = resultado.textContent + "1";
-    })
+function limpiar () {
+    document.getElementById("display").innerHTML = "";
+}
 
-    dos.addEventListener("click", function(){
-        resultado.textContent = resultado.textContent + "2";
-    })
+function back () {
+    let resultado = document.getElementById("display").innerHTML;
+    document.getElementById("display").innerHTML = resultado.substring(0, resultado.length - 1)
+}
 
-    tres.addEventListener("click", function(){
-        resultado.textContent = resultado.textContent + "3";
-    })
-
-    cuatro.addEventListener("click", function(){
-        resultado.textContent = resultado.textContent + "4";
-    })
-
-    cinco.addEventListener("click", function(){
-        resultado.textContent = resultado.textContent + "5";
-    })
-
-    seis.addEventListener("click", function(){
-        resultado.textContent = resultado.textContent + "6";
-    })
-
-    siete.addEventListener("click", function(){
-        resultado.textContent = resultado.textContent + "7";
-    })
-
-    ocho.addEventListener("click", function(){
-        resultado.textContent = resultado.textContent + "8";
-    })
-
-    nueve.addEventListener("click", function(){
-        resultado.textContent = resultado.textContent + "9";
-    })
-
-    cero.addEventListener("click", function(){
-        resultado.textContent = resultado.textContent + "0";
-    })
-
-    reset.addEventListener("click", function() {
-        resetear();
-    })
-
-    suma.addEventListener("click", function(){
-        operandoA = resultado.textContent;
-        operacion = "+";
-        limpiar();
-    })
-
-    resta.addEventListener("click", function(){
-        operandoA = resultado.textContent;
-        operacion = "-";
-        limpiar();
-    })
-
-    multiplicacion.addEventListener("click", function(){
-        operandoA = resultado.textContent;
-        operacion = "*";
-        limpiar();
-    })
-
-    division.addEventListener("click", function(){
-        operandoA = resultado.textContent;
-        operacion = "/";
-        limpiar();
-    })
-
-    igual.addEventListener("click", function(){
-        operandoB = resultado.textContent;
-        resolver();
-    })
-
-    function limpiar () {
-        resultado.textContent = "";
+function calcular () {
+    resultado = document.getElementById("display").innerHTML;
+    if (resultado) {
+        document.getElementById("display").innerHTML = eval(resultado);
     }
-    
-    function resetear () {
-        resultado.textContent = "";
-        operandoA = 0;
-        operandoB = 0;
-        operacion = "";
+    else {
+        document.getElementById("display").innerHTML = "Nada para calcular";
     }
+}
 
-function resolver () {
-    let res = 0;
-    if (operacion == "+") {
-        res = parseFloat(operandoA) + parseFloat(operandoB);
+function resultadoLength () {
+    resultado = JSON.stringify(resultado);
+
+    if (resultado.length > 8) {
+        resultado = JSON.parse(resultado);
+        document.getElementById("display").innerHTML = resultado.toFixed(5);
     }
-    else if (operacion == "-") {
-        res = parseFloat(operandoA) - parseFloat(operandoB);
-    }
-    else if (operacion == "*") {
-        res = parseFloat(operandoA) * parseFloat(operandoB);
-    }
-    else if (operacion == "/") {
-        res = parseFloat(operandoA) / parseFloat(operandoB);
-    }
-    resetear();
-    resultado.textContent = res;
 }
 
 const themeToggleBtn = document.querySelector(".theme-toggler");
@@ -127,7 +36,7 @@ const calculator = document.querySelector(".calculator");
 const toggleIcon = document.querySelector(".toggler-icon");
 let isDark = true;
 themeToggleBtn.onclick = () => {
-  calculator.classList.toggle("dark");
-  themeToggleBtn.classList.toggle("active");
-  isDark = !isDark;
-};
+calculator.classList.toggle("dark");
+themeToggleBtn.classList.toggle("active");
+isDark = !isDark;
+}
