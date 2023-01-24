81228b683660705cda269d4f0b1dbc80e28f0c4d
Francisco Vecchia <franvecchia00@gmail.com>
2023-01-24 18:59:37 -0300

    entrega-final

--- proyecto-prueba.js
+++ proyecto-prueba.js
@@ -7,18 +7,18 @@ function limpiar () {
     document.getElementById("display").innerHTML = "";
 }
 
-function back () {
+function back () { 
     let resultado = document.getElementById("display").innerHTML;
-    document.getElementById("display").innerHTML = resultado.substring(0, resultado.length - 1)
+    document.getElementById("display").innerHTML = resultado.substring(0, resultado.length - 1);
 }
 
 function calcular () {
     resultado = document.getElementById("display").innerHTML;
     if (resultado) {
         document.getElementById("display").innerHTML = eval(resultado);
-    }
-    else {
-        document.getElementById("display").innerHTML = "Nada para calcular";
+    } else {
+        display.innerHTML = "Vacío!";
+        setTimeout(() => (display.innerHTML = ""), 1000);
     }
 }
 
@@ -33,10 +33,41 @@ function resultadoLength () {
 
 const botonCambio = document.querySelector(".theme-toggler");
 const calculadora = document.querySelector(".calculator");
-const alternarIcono = document.querySelector(".toggler-icon");
-let oscuro = true;
-botonCambio.addEventListener("click", function() {
-calculadora.classList.toggle("dark");
-botonCambio.classList.toggle("active");
-oscuro = !oscuro;
+
+botonCambio.addEventListener("click", () => {
+    calculadora.classList.toggle("dark");
+    botonCambio.classList.toggle("active");
+
+    if(calculadora.classList.contains("dark")){
+        localStorage.setItem("modo-oscuro","true");
+
+    } else {
+        localStorage.setItem("modo-oscuro", "false");
+    }
 })
+
+if (localStorage.getItem("modo-oscuro") === "true") {
+    calculadora.classList.add("dark");
+    botonCambio.classList.add("active");
+} else {
+    calculadora.classList.remove("dark");
+    botonCambio.classList.remove("active");
+}
+
+let url = 'https://jsonplaceholder.typicode.com/users/';
+fetch(url)
+    .then( response => response.json())
+    .then( data => mostrarData(data))
+    .catch( error => console.log(error))
+
+const mostrarData = (data) => {
+    let body = "";
+    for (let i = 0; i < data.length; i++) {
+        body+= `<tr class="user-table">
+                    <td>${data[i].id}</td>
+                    <td>${data[i].name}</td>
+                    <td>${data[i].email}</td>
+                </tr>`
+    }
+    document.getElementById('data').innerHTML = body;
+}
